9bc1e1fe3c8a70ded982043e092c432fed161b08
khabindra <pujakhabi78@gmail.com>
2024-06-12 17:00:17 +0545

    Fetched data from database and displayed post in landing page

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', landing_page, name='landing_page'),
+    path('', LandingPageView.as_view(), name='landing_page'),
     path('posts', posts, name='posts_page'),
     path('posts/<slug:slug>/', post_detail, name='detail_posts_page'),
     path('contact/',Contactview,name='contact_page'),
--- my_blog/views.py
+++ my_blog/views.py
@@ -1,8 +1,23 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Post
 # Create your views here.
-def landing_page(request):
-    return render(request,'my_blog/index.html')
+
+
+class LandingPageView(ListView):
+    template_name = 'my_blog/index.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:2]
+        return data
+    
+
 def posts(request):
    pass
 
