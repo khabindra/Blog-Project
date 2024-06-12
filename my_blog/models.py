c5f52da7b96e86a50f2ad3a2692183b365e99dba
khabindra <pujakhabi78@gmail.com>
2024-06-12 15:45:05 +0545

    created a POST Models

--- my_blog/models.py
+++ my_blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User 
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    token = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='',null=True)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
